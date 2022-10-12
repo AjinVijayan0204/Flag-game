d94b2a3f11ac13cd508667a8b7fcd7dd8c4973b1
AjinVijayan0204 <ajinvijayan0204@gmail.com>
2022-10-12 22:09:29 +0530

    Initial Commit

---
+++ FlagGame/FlagGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGameApp.swift
+//  FlagGame
+//
+//  Created by Ajin on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FlagGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
